344f74a1209ee2416bc6a38cdbf1f119f1a40679
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-18 14:42:37 +0300

    feat: display books from data source on page load

--- library-project/main.js
+++ library-project/main.js
@@ -1,2 +1,8 @@
 import './modal.js'
 import './form.js'
+import { BookFactory } from './book/BookFactory.js'
+
+function showBooks() {
+  BookFactory.renderBooks()
+}
+window.addEventListener('DOMContentLoaded', showBooks)
