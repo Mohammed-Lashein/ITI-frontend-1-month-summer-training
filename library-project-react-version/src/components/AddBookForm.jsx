24180561a97108c641af9fb03752adeb91d991c4
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-19 15:34:59 +0300

    refactor: move AddBookForm markup from the Modal component to their dedicated component

--- library-project-react-version/src/components/AddBookForm.jsx
+++ library-project-react-version/src/components/AddBookForm.jsx
@@ -1,6 +1,50 @@
 function AddBookForm() {
-  return (
-    <div>AddBookForm</div>
-  )
+	return (
+		<form
+			action=''
+			className='add-new-book-form-container'
+		>
+			<header>Add new book</header>
+			<div className='form-group'>
+				<input
+					type='text'
+					name='book_title'
+					placeholder='Title'
+				/>
+			</div>
+
+			<div className='form-group'>
+				<input
+					type='text'
+					name='book_author'
+					placeholder='Author'
+				/>
+			</div>
+
+			<div className='form-group'>
+				<input
+					type='number'
+					name='book_pages_number'
+					maxLength='1000'
+					placeholder='Number of Pages'
+				/>
+			</div>
+
+			<div className='form-group'>
+				<label htmlFor='is_read'>Have you read it?</label>
+				<input
+					type='checkbox'
+					name='is_read'
+					id='is_read'
+				/>
+			</div>
+
+			<input
+				type='submit'
+				value='Submit'
+				className='btn btn-light-green'
+			/>
+		</form>
+	)
 }
 export default AddBookForm
--- library-project-react-version/src/components/Modal.jsx
+++ library-project-react-version/src/components/Modal.jsx
@@ -1,3 +1,5 @@
+import AddBookForm from './AddBookForm'
+
 function Modal({ isOpen, setIsOpen }) {
 	function closeModal(e) {
     if(e.target === e.currentTarget) {
@@ -10,51 +12,7 @@ function Modal({ isOpen, setIsOpen }) {
 		<>
 			{isOpen && (
 				<div className='modal' onClick={(e) => closeModal(e)}>
-					<form
-						action=''
-						className='add-new-book-form-container'
-					>
-						<header>Add new book</header>
-						<div className='form-group'>
-							<input
-								type='text'
-								name='book_title'
-								placeholder='Title'
-							/>
-						</div>
-
-						<div className='form-group'>
-							<input
-								type='text'
-								name='book_author'
-								placeholder='Author'
-							/>
-						</div>
-
-						<div className='form-group'>
-							<input
-								type='number'
-								name='book_pages_number'
-								maxLength='1000'
-								placeholder='Number of Pages'
-							/>
-						</div>
-
-						<div className='form-group'>
-							<label htmlFor='is_read'>Have you read it?</label>
-							<input
-								type='checkbox'
-								name='is_read'
-								id='is_read'
-							/>
-						</div>
-
-						<input
-							type='submit'
-							value='Submit'
-							className='btn btn-light-green'
-						/>
-					</form>
+					<AddBookForm />
 				</div>
 			)}
 		</>
