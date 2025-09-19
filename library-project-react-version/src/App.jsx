417e0fb4eee34b14ce83b21ca18c4d56b71c0ac0
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-19 11:12:39 +0300

    refactor: format App component

--- library-project-react-version/src/App.jsx
+++ library-project-react-version/src/App.jsx
@@ -2,10 +2,12 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
 function App() {
-  return <div>
-    <Navbar />
-    <Footer />
-  </div>
+	return (
+		<div>
+			<Navbar />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
