d56913abddea8baca1c5c68cfe8626cda1ff069b
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-19 15:33:10 +0300

    feat: add the initial markup of Book component

--- library-project-react-version/src/components/Book.jsx
+++ library-project-react-version/src/components/Book.jsx
@@ -1,6 +1,12 @@
-function Book() {
-  return (
-    <div>Book</div>
-  )
+function Book({title, author, pagesNumber, isRead}) {
+	return (
+		<article class='book'>
+			<p class='book__title'>{title}</p>
+			<p class='book__author'>{author}</p>
+			<p class='book__pages-number'>{pagesNumber}</p>
+			<button class={`btn ${isRead ? 'btn-light-green' : 'btn-light-red'}`}>{isRead ? 'Read' : 'Not Read'}</button>
+			<button class='btn'>Remove</button>
+		</article>
+	)
 }
 export default Book
