911dd46b2974503bedab85d0b578d26875ca4cb1
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-19 11:38:04 +0300

    feat: conditionally render the Modal based on a state instead of toggling 'hidden' class

--- library-project-react-version/src/App.jsx
+++ library-project-react-version/src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import AddBookButton from './components/AddBookButton'
 import Footer from './components/Footer'
 import Modal from './components/Modal'
 import Navbar from './components/Navbar'
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
 	return (
 		<div>
 			<Navbar />
       <AddBookButton />
-      <Modal />
+      <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}/>
 			<Footer />
 		</div>
 	)
--- library-project-react-version/src/components/Modal.jsx
+++ library-project-react-version/src/components/Modal.jsx
@@ -1,49 +1,56 @@
-function Modal() {
+function Modal({ isOpen, setIsOpen }) {
 	return (
-		<div className='modal hidden'>
-			<form
-				action=''
-				className='add-new-book-form-container'
-			>
-				<header>Add new book</header>
-				<div className='form-group'>
-					<input
-						type='text'
-						name='book_title'
-						placeholder='Title'
-					/>
-				</div>
+		<>
+			{isOpen && (
+				<div className='modal'>
+					<form
+						action=''
+						className='add-new-book-form-container'
+					>
+						<header>Add new book</header>
+						<div className='form-group'>
+							<input
+								type='text'
+								name='book_title'
+								placeholder='Title'
+							/>
+						</div>
 
-				<div className='form-group'>
-					<input
-						type='text'
-						name='book_author'
-						placeholder='Author'
-					/>
+						<div className='form-group'>
+							<input
+								type='text'
+								name='book_author'
+								placeholder='Author'
+							/>
+						</div>
+
+						<div className='form-group'>
+							<input
+								type='number'
+								name='book_pages_number'
+								maxLength='1000'
+								placeholder='Number of Pages'
+							/>
+						</div>
+
+						<div className='form-group'>
+							<label htmlFor='is_read'>Have you read it?</label>
+							<input
+								type='checkbox'
+								name='is_read'
+								id='is_read'
+							/>
+						</div>
+
+						<input
+							type='submit'
+							value='Submit'
+							className='btn btn-light-green'
+						/>
+					</form>
 				</div>
-				<div className='form-group'>
-					<input
-						type='number'
-						name='book_pages_number'
-						maxLength='1000'
-						placeholder='Number of Pages'
-					/>
-				</div>
-				<div className='form-group'>
-					<label htmlFor='is_read'>Have you read it?</label>
-					<input
-						type='checkbox'
-						name='is_read'
-						id='is_read'
-					/>
-				</div>
-				<input
-					type='submit'
-					value='Submit'
-					className='btn btn-light-green'
-				/>
-			</form>
-		</div>
+			)}
+		</>
 	)
 }
 export default Modal
