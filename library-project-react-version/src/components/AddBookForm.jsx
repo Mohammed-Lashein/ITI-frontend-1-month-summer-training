2def348f770505b61f872289b3a8ed1d89bb46fc
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-20 17:57:39 +0300

    refactor(AddBookForm): remove unused state + accept addBook() instead of calling useBooks() within the component

--- library-project-react-version/src/components/AddBookForm.jsx
+++ library-project-react-version/src/components/AddBookForm.jsx
@@ -1,25 +1,21 @@
-import { useRef, useState } from 'react'
-import { bookIdGenerator } from '../utils'
+import { useRef } from 'react'
 
-function AddBookForm() {
-	const [bookTitle, setBookTitle] = useState('')
-	const [bookAuthor, setBookAuthor] = useState('')
-	const [bookPagesNumber, setBookPagesNumber] = useState(0)
-	const [isRead, setIsRead] = useState(false)
+function AddBookForm({setIsModalOpen, addBook}) {
 
 	const bookTitleRef = useRef(null)
 	const bookAuthorRef = useRef(null)
 	const bookPagesNumberRef = useRef(null)
 	const isBookReadRef = useRef(null)
+	
 	function handleSubmission(e) {
 		e.preventDefault()
 
-		setBookTitle(bookTitleRef.current.value)
-		setBookAuthor(bookAuthorRef.current.value)
-		setBookPagesNumber(bookPagesNumberRef.current.value)
-		setIsRead(isBookReadRef.current.checked)
-
-		const aFieldIsEmpty = bookTitle === '' || bookAuthor === '' || bookPagesNumber === 0
+		// since state values will reflect in the next UI print, manually checking them is wrong.
+		/* 
+			I noticed so when I submit the form for the 1st time, I get the message saying 
+			"All fields are required". I need to click a 2nd time in order for the updated state to take effect
+		*/
+		const aFieldIsEmpty = bookTitleRef.current.value === '' || bookAuthorRef.current.value === '' || bookPagesNumberRef.current.value === 0
 		if (aFieldIsEmpty) {
 			console.log('All fields are required') // the bookPagesNumber should be greater than zero, but let's
 			// keep that for a later better validation approach
@@ -29,15 +25,17 @@ function AddBookForm() {
 		console.log('end of fn reached!')
 
 		// update data source with data
-		const book = {
-			id: bookIdGenerator(),
-			title: bookTitle,
-			author: bookAuthor,
-			pagesNumber: bookPagesNumber,
-			isRead: isRead,
+		const formElementsValues = {
+			title: bookTitleRef.current.value,
+			author: bookAuthorRef.current.value,
+			pagesNumber: bookPagesNumberRef.current.value,
+			isRead: isBookReadRef.current.checked,
 		}
-		updateDataSource(book)
 		// update books state to trigger a re-render. Deferred till we lift the state up
+
+		addBook(formElementsValues)
+		setIsModalOpen(false)
+
 	}
 	function updateDataSource(book) {
 		const books = JSON.parse(sessionStorage.getItem('books')) || []
