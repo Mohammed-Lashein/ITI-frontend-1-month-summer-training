ff8b319ffe31f86f6d7fcc396631be934f073ad7
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-20 18:42:28 +0300

    feat(Book): add the wrong (will be fixed later but kept for educational purposes) toggleReadStatus functionality

--- library-project-react-version/src/components/Book.jsx
+++ library-project-react-version/src/components/Book.jsx
@@ -6,12 +6,27 @@ function Book({setBooks, book: {id, title, author, pagesNumber, isRead}}) {
 		setBooks(updatedBooks)
 		BookMapper.updateBooks(updatedBooks)
 	}
+	function toggleReadStatus(bookId) {
+		console.log('this is bookId', bookId)
+		const books = BookMapper.getBooks() // You are creating new books! This is wrong in React. Stick to the state
+		const updatedBooks = books.map((book) => {
+			if(book.id === bookId) {
+				return {
+					...book,
+					isRead: !book.isRead
+				}
+			}
+			return book
+		})
+		setBooks(updatedBooks)
+		BookMapper.updateBooks(updatedBooks)
+	}
 	return (
 		<article className='book'>
 			<p className='book__title'>{title}</p>
 			<p className='book__author'>{author}</p>
 			<p className='book__pages-number'>{pagesNumber}</p>
-			<button className={`btn ${isRead ? 'btn-light-green' : 'btn-light-red'}`}>{isRead ? 'Read' : 'Not Read'}</button>
+			<button className={`btn ${isRead ? 'btn-light-green' : 'btn-light-red'}`} onClick={() => toggleReadStatus(id)}>{isRead ? 'Read' : 'Not Read'}</button>
 			<button className='btn' onClick={() => removeBook(id)}>Remove</button>
 		</article>
 	)
