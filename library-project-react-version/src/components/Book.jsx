f3c2833f2610cbb861dc18b6f4bc2d23d178ecdd
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-20 18:15:06 +0300

    feat: add removeBook functionality

--- library-project-react-version/src/components/Book.jsx
+++ library-project-react-version/src/components/Book.jsx
@@ -1,11 +1,17 @@
-function Book({book: {title, author, pagesNumber, isRead}}) {
+import { BookMapper } from '../utils/BookMapper';
+function Book({book: {id, title, author, pagesNumber, isRead}}) {
+	function removeBook(id) {
+		const books = BookMapper.getBooks()
+		const updatedBooks = books.filter((book) => book.id !== id)
+		BookMapper.updateBooks(updatedBooks)
+	}
 	return (
 		<article className='book'>
 			<p className='book__title'>{title}</p>
 			<p className='book__author'>{author}</p>
 			<p className='book__pages-number'>{pagesNumber}</p>
 			<button className={`btn ${isRead ? 'btn-light-green' : 'btn-light-red'}`}>{isRead ? 'Read' : 'Not Read'}</button>
-			<button className='btn'>Remove</button>
+			<button className='btn' onClick={() => removeBook(id)}>Remove</button>
 		</article>
 	)
 }
