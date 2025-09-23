b3633602b8daec7a95b44739208b5fdc0319e160
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-23 14:46:59 +0300

    feat: create CartContext

---
+++ final-project/src/context/CartContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useState } from 'react'
+
+export const CartContext = createContext({})
+
+export function CartProvider({ children }) {
+	const [cartItems, setCartItems] = useState(JSON.parse(sessionStorage.getItem('cart')) || [])
+
+	const cartQuantity = cartItems.reduce((quantity, item) => item?.quantity + quantity, 0)
+
+	function getItemQuantity(id) {
+		// if we don't use the optional chaining operator, since we are not in TS, nothing will happen
+		return cartItems.find((item) => item.id === id).quantity || 0
+	}
+	function increaseCartQuantity(id) {
+		const itemExistsInCart = cartItems.find((item) => item.id === id)
+
+		if (itemExistsInCart) {
+			const updatedItems = cartItems.map((item) => {
+				if (item.id === id) {
+					return { ...item, quantity: item.quantity + 1 }
+				}
+				return item
+			})
+			setCartItems(updatedItems)
+		}
+
+		if (!itemExistsInCart) {
+			setCartItems((prev) => [...prev, { id, quantity: 1 }])
+		}
+	}
+	function decreaseCartQuantity(id) {
+		const itemIsOfQuantityOne = cartItems.find((item) => item.id === id)?.quantity === 1
+
+		if (itemIsOfQuantityOne) {
+			const updatedCartItems = cartItems.filter((item) => item.id !== id)
+			setCartItems(updatedCartItems)
+		}
+
+		if (!itemIsOfQuantityOne) {
+			const updatedCartItems = cartItems.map((item) => {
+				if (item.id === id) {
+					return {
+						...item,
+						quantity: item.quantity - 1,
+					}
+				}
+				return item
+			})
+			setCartItems(updatedCartItems)
+		}
+	}
+	function removeFromCart(id) {
+		setCartItems((currItems) => {
+			return currItems.filter((item) => item.id !== id)
+		})
+	}
+	return (
+		<CartContext.Provider
+			value={{
+				getItemQuantity,
+				increaseCartQuantity,
+				decreaseCartQuantity,
+				removeFromCart,
+				cartItems,
+				cartQuantity,
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	)
+}
---
+++ final-project/src/context/README.md
@@ -0,0 +1,4 @@
+# Cart Context
+I borrowed this code from this [amazing video](https://www.youtube.com/watch?v=lATafp15HWA) that I studied before. It mainly uses TypeScript so it is a great opportunity if you want to learn React + TypeScript.  
+
+I have modified the code since our project is just plain js.
